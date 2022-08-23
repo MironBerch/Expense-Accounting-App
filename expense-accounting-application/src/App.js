c40506fe84b9d3612201140926bbde5d55b80868
MironBerch <mironberchetov@gmail.com>
2022-08-23 17:03:06 +0300

    app

---
+++ expense-accounting-application/src/App.js
@@ -0,0 +1,13 @@
+import CostItem from './components/CostItem';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <CostItem></CostItem>
+    </div>
+  );
+}
+
+export default App;
